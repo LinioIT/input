0908557511bc0135b533cea38aacfff3b5e339b2
Igor Nóbrega <igordouradonobrega@gmail.com>
2017-08-25 13:26:41 -0500

    Switched to switch checking types

--- src/Constraint/Type.php
+++ src/Constraint/Type.php
@@ -20,12 +20,13 @@ class Type extends Constraint
 
     public function validate($content): bool
     {
-        if ($this->type === 'datetime') {
-            $date = date_parse($content);
+        switch ($this->type) {
+            case 'datetime':
+                $date = date_parse($content);
 
-            return $date['error_count'] ? false : true;
+                return $date['error_count'] ? false : true;
+            default:
+                return call_user_func('is_' . $this->type, $content);
         }
-
-        return call_user_func('is_' . $this->type, $content);
     }
 }
